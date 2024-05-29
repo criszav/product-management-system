671414460cc1e77f13e245c79ffc885f597f966e
criszav <czavala.dev@gmail.com>
2024-05-29 10:05:21 -0400

    add authentication manager

---
+++ src/main/java/com/czavala/productmanagementsystem/config/security/ApplicationConfig.java
@@ -0,0 +1,48 @@
+package com.czavala.productmanagementsystem.config.security;
+
+import com.czavala.productmanagementsystem.exceptions.ResourceNotFoundException;
+import com.czavala.productmanagementsystem.persistance.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        // configura la estrategia de autenticacion
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(); // es una implementacion del AuthenticationProvider
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found. Username: " + username));
+    }
+}
