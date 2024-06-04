ce6d00add328e7315fa29da92c31162a8d52b120
criszav <czavala.dev@gmail.com>
2024-06-03 21:38:29 -0400

    add methods: logout, find logged in user, save token in db

--- src/main/java/com/czavala/productmanagementsystem/services/auth/AuthenticationService.java
+++ src/main/java/com/czavala/productmanagementsystem/services/auth/AuthenticationService.java
@@ -4,18 +4,25 @@ import com.czavala.productmanagementsystem.dto.auth.AuthRequestDto;
 import com.czavala.productmanagementsystem.dto.auth.AuthResponseDto;
 import com.czavala.productmanagementsystem.dto.auth.RegisteredUserDto;
 import com.czavala.productmanagementsystem.dto.auth.SaveRegisterUserDto;
+import com.czavala.productmanagementsystem.exceptions.ResourceNotFoundException;
+import com.czavala.productmanagementsystem.persistance.entities.JwtToken;
 import com.czavala.productmanagementsystem.persistance.entities.User;
+import com.czavala.productmanagementsystem.persistance.repository.JwtTokenRepository;
 import com.czavala.productmanagementsystem.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,10 +31,16 @@ public class AuthenticationService {
     private final UserService userService;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final JwtTokenRepository jwtTokenRepository;
 
     public RegisteredUserDto registerCustomer(SaveRegisterUserDto saveRegisterUserDto) {
 
         User user = userService.registerOneCustomer(saveRegisterUserDto); // realiza el registro de un nuevo customer
+
+        String jwt = jwtService.generateToken(user, generateExtraClaims(user)); // genera token a partir de los datos del user registrado
+
+        // Guarda token en DB (para realizacion de logout y desactivar token en DB)
+        saveUserToken(user, jwt);
 
         RegisteredUserDto userDto = new RegisteredUserDto();
         userDto.setId(user.getId());
@@ -35,11 +48,20 @@ public class AuthenticationService {
         userDto.setLastname(user.getLastname());
         userDto.setUsername(user.getUsername());
         userDto.setRole(user.getRole().name());
-
-        String jwt = jwtService.generateToken(user, generateExtraClaims(user));
         userDto.setJwt(jwt);
 
         return userDto;
+    }
+
+    private void saveUserToken(User user, String jwt) {
+        // Crea un token para guardarlo en DB
+        JwtToken token = new JwtToken();
+        token.setToken(jwt);
+        token.setUser(user);
+        token.setExpiration(jwtService.extractExpirationDate(jwt));
+        token.setValid(true);
+        token.setTimestamp(LocalDateTime.now());
+        jwtTokenRepository.save(token);
     }
 
     // genera claims extras que son agregados al payload del token
@@ -74,6 +96,9 @@ public class AuthenticationService {
         // genera token con los datos del user (que seacaba de autenticar) obtenidos previamente
         String jwt = jwtService.generateToken(user, generateExtraClaims((User) user));
 
+        // Guarda token en DB (para realizacion de logout y desactivar token en DB)
+        saveUserToken((User) user, jwt);
+
         // crea el dto para enviar el jwt de vuelta del user recien autenticado
         AuthResponseDto authResponseDto = new AuthResponseDto();
         authResponseDto.setJwt(jwt);
@@ -101,5 +126,39 @@ public class AuthenticationService {
             System.out.println(e.getMessage());
             return false;
         }
+    }
+
+    public User findLoggedInUser() {
+
+        // obtiene el objeto de autenticacion
+        Authentication authentication = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+
+        // extrae desde el objeto de autenticacion, el usermame de user autenticado actualmentew
+        String username = (String) authentication.getPrincipal();
+
+        return userService.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found. Username: " + username));
+    }
+
+    public void logout(HttpServletRequest request) {
+
+        String jwt = jwtService.extractJwtFromRequest(request);
+
+        // Verifica si el token extraido previamente de la solicitud, contiene texto o si es nulo
+        // Si es nulo o si no contiene texto (el token), entonces retorna el control a la cadena de filtros
+        if (jwt == null || !StringUtils.hasText(jwt)) return;
+
+        // Si la solicitud efectivamente contiene un token, entonces buscamos ese token en la DB
+        Optional<JwtToken> optJwtToken = jwtTokenRepository.findByToken(jwt);
+
+        // Luego de encontrar el token en DB, debemos deshabilitar ese token, ya que user esta realizando logout
+        if (optJwtToken.isPresent() && optJwtToken.get().isValid()) {
+            // Deshabilita la validez del token (isValid = false)
+            optJwtToken.get().setValid(false);
+            // Guarda cambios del token en DB
+            jwtTokenRepository.save(optJwtToken.get());
+        }
+
+
     }
 }
