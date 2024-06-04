f909bf5c2f21a2ab8d555de2f419098079a873e9
criszav <czavala.dev@gmail.com>
2024-06-03 21:35:04 -0400

    change login endpoint for '/authenticate', '/validate' for token validation, add user profile endpoint

--- src/main/java/com/czavala/productmanagementsystem/controller/AuthenticationController.java
+++ src/main/java/com/czavala/productmanagementsystem/controller/AuthenticationController.java
@@ -1,12 +1,17 @@
 package com.czavala.productmanagementsystem.controller;
 
+import com.czavala.productmanagementsystem.dto.LogoutResponse;
 import com.czavala.productmanagementsystem.dto.auth.AuthRequestDto;
 import com.czavala.productmanagementsystem.dto.auth.AuthResponseDto;
+import com.czavala.productmanagementsystem.persistance.entities.User;
 import com.czavala.productmanagementsystem.services.auth.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/auth")
@@ -15,16 +20,29 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
-    @PostMapping("/login")
+    @PostMapping("/authenticate")
     public ResponseEntity<AuthResponseDto> login(@RequestBody @Valid AuthRequestDto authRequestDto) {
         AuthResponseDto authResponse = authenticationService.login(authRequestDto);
         return ResponseEntity.ok(authResponse);
     }
 
     // endpoint para visualizar la validez de un jwt
-    @GetMapping("/validate-token")
+    @GetMapping("/validate")
     public ResponseEntity<Boolean> validateToken(@RequestParam String jwt) {
         boolean isTokenValid = authenticationService.validateToken(jwt);
         return ResponseEntity.ok(isTokenValid);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> findMyProfile() { // todo - retornar user dto en lugar de user entity
+        User user = authenticationService.findLoggedInUser();
+        System.out.println("User from AuthenticationController: " + user);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<LogoutResponse> logout(HttpServletRequest request) {
+        authenticationService.logout(request);
+        return ResponseEntity.ok(new LogoutResponse("Successfull logout.", LocalDateTime.now()));
     }
 }
