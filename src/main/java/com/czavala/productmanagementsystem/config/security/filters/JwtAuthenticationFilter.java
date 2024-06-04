d43e0fbf42d5f7cc7c7518efe455e0a625802080
criszav <czavala.dev@gmail.com>
2024-06-03 21:31:13 -0400

    add token validation, method to update token in db

--- src/main/java/com/czavala/productmanagementsystem/config/security/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/czavala/productmanagementsystem/config/security/filters/JwtAuthenticationFilter.java
@@ -1,7 +1,9 @@
 package com.czavala.productmanagementsystem.config.security.filters;
 
 import com.czavala.productmanagementsystem.exceptions.ResourceNotFoundException;
+import com.czavala.productmanagementsystem.persistance.entities.JwtToken;
 import com.czavala.productmanagementsystem.persistance.entities.User;
+import com.czavala.productmanagementsystem.persistance.repository.JwtTokenRepository;
 import com.czavala.productmanagementsystem.services.UserService;
 import com.czavala.productmanagementsystem.services.auth.JwtService;
 import jakarta.servlet.FilterChain;
@@ -11,11 +13,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -23,6 +28,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserService userService;
+    private final JwtTokenRepository jwtTokenRepository;
 
     /*
     * Verifica si la solicitid HTTP enviada por un usuario, contiene un token de autenticacion (jwt) válido
@@ -33,32 +39,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // 1. obtener header que contiene el bearer token ("Authorization")
+        // Extrae token JWT desde el request enviado por el user
+        String jwt = jwtService.extractJwtFromRequest(request);
 
-        // obtiene el header de autorizacion de la solicitud HTTP
-        // este header contiene el token jwt
-        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-
-        // 1.1 validar que header contenga texto y que comience con el prefijo 'Bearer '
-
-        // Verifica que header contenga texto y que comience con el prefijo 'Bearer '
-        // de lo contrario, retorna a la cadena de filtros, sin autenticacion
-        if (!StringUtils.hasText(authorizationHeader) || !authorizationHeader.startsWith("Bearer ")) {
+        // Verifica si el token extraido previamente de la solicitud, contiene texto o si es nulo
+        // Si es nulo o si no contiene texto (el token), entonces retorna el control a la cadena de filtros
+        if (jwt == null || !StringUtils.hasText(jwt)) {
             filterChain.doFilter(request, response);
+            return;
         }
 
-        // 2. obtener el token desde el authorization header
+        // Si la solicitud (requst) efectivamente contiene un token, entonces buscamos ese token en la DB
+        Optional<JwtToken> token = jwtTokenRepository.findByToken(jwt);
 
-        // Extrae el token jwt del header de autorizacion
-        // El header tiene formato 'Beaerer <token>', entonces divide la cadena en un espacio, y toma el seguno item (posicion 1), que es el token
-        String jwt = authorizationHeader.split(" ")[1];
+        boolean isValid = validateToken(token);
 
-        // 3. obtener el subject (username) desde el jwt
+        // Verifica que token obtenido previamente sea valido
+        // Si el token NO es valido, entonces retorno a la cadena de filtros de seguridad
+        if (!isValid) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         // Obtiene el username desde el token jwt
         String username = jwtService.extractUsername(jwt);
 
-        // 4. setear el objeto Authentication dentro del SecurityContextHolder
+        // Setea el objeto Authentication dentro del SecurityContextHolder
         User user = userService.findByUsername(username)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found. Username: " + username));
 
@@ -70,8 +76,37 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 user.getAuthorities() // permisos otorgados al user
         );
 
-        // 5. ejecuta el resto de filtros de la cadena
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        // Ejecuta el resto de filtros de la cadena
         filterChain.doFilter(request, response);
 
+    }
+
+    private boolean validateToken(Optional<JwtToken> token) {
+
+        // Si no encuentra token obtenido desde DB, automaticamente retorna 'false' (token NO valido)
+        // Lo anterior significa que el token o no existe o no fue generado por nuestro sistema
+        if (!token.isPresent()) {
+            return false;
+        }
+
+        JwtToken jwtToken = token.get(); // obtiene token
+        Date currentDate = new Date(System.currentTimeMillis()); // obtiene fecha actual del sistema
+
+        // Token será valido si su fecha de expiracion es posterior a la fecha actual del sistema
+        // es decir, aún no ha expirado el token (es valido)
+        boolean isValid = jwtToken.isValid() && jwtToken.getExpiration().after(currentDate);
+
+        // Si por el contrario, el token NO es valido, entonces actualizamos su estatus (el del jwt) en la DB (lo hacemos invalido)
+        if (!isValid) {
+            updateTokenStatus(jwtToken);
+        }
+        return isValid; // retorna validez del token (true o false)
+    }
+
+    private void updateTokenStatus(JwtToken jwtToken) {
+        jwtToken.setValid(false); // invalida el token en DB
+        jwtTokenRepository.save(jwtToken);
     }
 }
