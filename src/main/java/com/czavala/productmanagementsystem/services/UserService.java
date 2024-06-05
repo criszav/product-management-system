ce03261111daea67310bfc41902bde2876819dd4
criszav <czavala.dev@gmail.com>
2024-06-05 11:39:29 -0400

    add logic to update an user

--- src/main/java/com/czavala/productmanagementsystem/services/UserService.java
+++ src/main/java/com/czavala/productmanagementsystem/services/UserService.java
@@ -1,6 +1,8 @@
 package com.czavala.productmanagementsystem.services;
 
 import com.czavala.productmanagementsystem.dto.auth.SaveRegisterUserDto;
+import com.czavala.productmanagementsystem.dto.customer.CustomerDto;
+import com.czavala.productmanagementsystem.dto.customer.SaveCustomerDto;
 import com.czavala.productmanagementsystem.persistance.entities.User;
 
 import java.util.Optional;
@@ -10,4 +12,7 @@ public interface UserService {
     User registerOneCustomer(SaveRegisterUserDto saveRegisterUserDto);
 
     Optional<User> findByUsername(String username);
+
+    CustomerDto updateCustomer(Long id, SaveCustomerDto saveCustomerDto);
+
 }
--- src/main/java/com/czavala/productmanagementsystem/services/impl/UserServiceImpl.java
+++ src/main/java/com/czavala/productmanagementsystem/services/impl/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package com.czavala.productmanagementsystem.services.impl;
 
 import com.czavala.productmanagementsystem.dto.auth.SaveRegisterUserDto;
+import com.czavala.productmanagementsystem.dto.customer.CustomerDto;
+import com.czavala.productmanagementsystem.dto.customer.SaveCustomerDto;
 import com.czavala.productmanagementsystem.exceptions.InvalidPasswordException;
+import com.czavala.productmanagementsystem.exceptions.ResourceNotFoundException;
+import com.czavala.productmanagementsystem.mapper.UserMapper;
 import com.czavala.productmanagementsystem.persistance.Utils.Role;
 import com.czavala.productmanagementsystem.persistance.entities.User;
 import com.czavala.productmanagementsystem.persistance.repository.UserRepository;
@@ -20,6 +24,7 @@ public class UserServiceImpl implements UserService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @Override
     public User registerOneCustomer(SaveRegisterUserDto saveRegisterUserDto) {
@@ -54,4 +59,19 @@ public class UserServiceImpl implements UserService {
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
+
+    @Override
+    public CustomerDto updateCustomer(Long id, SaveCustomerDto saveCustomerDto) {
+        User customerFromDB = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found. Customer id: " + id));
+
+        customerFromDB.setFirstname(saveCustomerDto.getFirstname());
+        customerFromDB.setLastname(saveCustomerDto.getLastname());
+        customerFromDB.setUsername(saveCustomerDto.getUsername());
+        customerFromDB.setLastModified(LocalDateTime.now());
+        userRepository.save(customerFromDB);
+
+        return userMapper.mapToCustomerDto(customerFromDB);
+    }
+
 }
