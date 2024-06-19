d8cefd48db9b26dc35ad379b5869a12f1a041f0b
criszav <czavala.dev@gmail.com>
2024-06-19 16:06:06 -0400

    change validation for "Long " datatype: min value, add image field

--- src/main/java/com/czavala/productmanagementsystem/dto/SaveProductDto.java
+++ src/main/java/com/czavala/productmanagementsystem/dto/SaveProductDto.java
@@ -1,15 +1,14 @@
 package com.czavala.productmanagementsystem.dto;
 
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 
 @Getter
 @Setter
@@ -20,10 +19,10 @@ public class SaveProductDto implements Serializable {
     @NotBlank(message = "Product name is required")
     private String name;
 
-    @DecimalMin(value = "0.01", message = "Price must be greater than 0")
-    private BigDecimal price;
-
-//    private String imageUrl;
+    @Min(value = 1, message = "Price must be greater than 1")
+    private Long price;
+    
+    private MultipartFile image;
 
     @Min(value = 1, message = "Category must be valid")
     private Long categoryId;
