f6d212bdf1e9caf19c4f8b9bda1fce96b8aeba46
criszav <czavala.dev@gmail.com>
2024-06-24 11:34:00 -0400

    add find order by user id method

--- src/main/java/com/czavala/productmanagementsystem/persistance/repository/OrderRepository.java
+++ src/main/java/com/czavala/productmanagementsystem/persistance/repository/OrderRepository.java
@@ -3,5 +3,8 @@ package com.czavala.productmanagementsystem.persistance.repository;
 import com.czavala.productmanagementsystem.persistance.entities.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
 }
