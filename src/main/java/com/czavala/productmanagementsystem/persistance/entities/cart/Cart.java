ab9bc54c49cea8f869664ec595c723b6690d7e22
criszav <czavala.dev@gmail.com>
2024-07-01 12:45:25 -0400

    add fields to entity class: timestamps (created, updated), status, relation 1to1 with order entity

--- src/main/java/com/czavala/productmanagementsystem/persistance/entities/cart/Cart.java
+++ src/main/java/com/czavala/productmanagementsystem/persistance/entities/cart/Cart.java
@@ -1,12 +1,20 @@
 package com.czavala.productmanagementsystem.persistance.entities.cart;
 
+import com.czavala.productmanagementsystem.persistance.Utils.CartStatus;
 import com.czavala.productmanagementsystem.persistance.entities.User;
+import com.czavala.productmanagementsystem.persistance.entities.order.Order;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.CodePointLength;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +36,23 @@ public class Cart {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToOne(mappedBy = "cart")
+    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CartStatus status;
+
+    @CreatedDate
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    @Column(insertable = false)
+    private LocalDateTime lastModified;
 
     @Transient
     public Long getTotalCart() {
