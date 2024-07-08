20df4997753f6641aa3b33bfe9c5e7615aba5e32
criszav <czavala.dev@gmail.com>
2024-07-08 08:59:19 -0400

    Change query to find cart of authenticated user
    Status "NEW" (of the cart) was added to the query in the where clause

--- src/main/java/com/czavala/productmanagementsystem/persistance/repository/CartRepository.java
+++ src/main/java/com/czavala/productmanagementsystem/persistance/repository/CartRepository.java
@@ -2,10 +2,12 @@ package com.czavala.productmanagementsystem.persistance.repository;
 
 import com.czavala.productmanagementsystem.persistance.entities.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    Optional<Cart> findByUserId(Long userId);
+    @Query("SELECT c FROM Cart c WHERE c.user.id = :userId AND c.status = 'NEW'")
+    Optional<Cart> findByUserIdAndStatusNew(Long userId);
 }
