4536847b0e680bd098faae00d5081dbaefa25b83
criszav <czavala.dev@gmail.com>
2024-07-08 09:02:32 -0400

    change parameter to find cart of authenticated user
    parameter changed from "user" (entity) to username

--- src/main/java/com/czavala/productmanagementsystem/services/impl/CartServiceImpl.java
+++ src/main/java/com/czavala/productmanagementsystem/services/impl/CartServiceImpl.java
@@ -4,6 +4,7 @@ import com.czavala.productmanagementsystem.dto.cart.AddProductToCartRequest;
 import com.czavala.productmanagementsystem.dto.cart.CartDto;
 import com.czavala.productmanagementsystem.exceptions.ResourceNotFoundException;
 import com.czavala.productmanagementsystem.mapper.CartMapper;
+import com.czavala.productmanagementsystem.persistance.Utils.CartStatus;
 import com.czavala.productmanagementsystem.persistance.entities.Product;
 import com.czavala.productmanagementsystem.persistance.entities.User;
 import com.czavala.productmanagementsystem.persistance.entities.cart.Cart;
@@ -12,8 +13,11 @@ import com.czavala.productmanagementsystem.persistance.repository.CartItemReposi
 import com.czavala.productmanagementsystem.persistance.repository.CartRepository;
 import com.czavala.productmanagementsystem.persistance.repository.ProductRepository;
 import com.czavala.productmanagementsystem.services.CartService;
+import com.czavala.productmanagementsystem.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -23,18 +27,23 @@ public class CartServiceImpl implements CartService {
     private final CartMapper cartMapper;
     private final ProductRepository productRepository;
     private final CartItemRepository cartItemRepository;
+    private final UserService userService;
 
     @Override
-    public CartDto getCurrentUserCart(User user) {
+    public CartDto getCurrentUserCart(String username) {
+        User user = userService.findByUsername(username).get();
         Cart cartFromDB = getOrCreateCartByUserId(user);
         return cartMapper.mapToCartDto(cartFromDB);
     }
 
     @Override
-    public CartDto addItemToCart(User user, AddProductToCartRequest addProductRequest) {
+    public CartDto addItemToCart(String username, AddProductToCartRequest addProductRequest) {
 
         Long productId = addProductRequest.getProductId();
         Integer productQuantity = addProductRequest.getQuantity();
+
+        // Obtiene user autenticado
+        User user = userService.findByUsername(username).get();
 
         // Obtener carrito del user, o crear uno si aún no tiene uno
         Cart userCart = getOrCreateCartByUserId(user);
@@ -48,7 +57,9 @@ public class CartServiceImpl implements CartService {
         cartItem.setCart(userCart);
         cartItem.setProduct(product);
         cartItem.setQuantity(productQuantity);
-        // todo - crear repositorio para guardar CartItem (aunque no sé si es necesario)
+        // todo - crear repositorio para guardar CartItem (aunque no sé si es necesario) -> (actualizacion 24/06) SI ERA NECESARIO
+        // todo: De lo contrario, no guarda los items en el carrito
+        cartItemRepository.save(cartItem);
 
         // Agrega a la lista de items del cart, el CartItem recien creado
         userCart.getCartItems().add(cartItem);
@@ -60,7 +71,9 @@ public class CartServiceImpl implements CartService {
     }
 
     @Override
-    public CartDto removeItemFromCart(User user, Long productId) {
+    public CartDto removeItemFromCart(String username, Long productId) {
+
+        User user = userService.findByUsername(username).get();
 
         // Obtiene cart del usuario autenticado (sobre el que se quiere eliminar un producto)
         Cart cartFromDB = getCartByUser(user);
@@ -73,7 +86,10 @@ public class CartServiceImpl implements CartService {
                 .orElseThrow(() -> new ResourceNotFoundException("Item not found in cart. Item id: " + productId));
 
         cartFromDB.getCartItems().remove(cartItem);
-//        cartItemRepository.delete(cartItem); // elimina el cart item del repositorio (aun no definido si esto va o no)
+
+        // elimina el cart item del repositorio (aun no definido si esto va o no)
+        // Esto debe ir, ya que de lo contrario, el cart no se actualiza
+        cartItemRepository.delete(cartItem);
 
         cartRepository.save(cartFromDB);
         return cartMapper.mapToCartDto(cartFromDB);
@@ -82,17 +98,19 @@ public class CartServiceImpl implements CartService {
     // Busca Cart de usuario autenticado
     // Si no encuentra un Cart, crea uno por defecto
     private Cart getOrCreateCartByUserId(User user) {
-        return cartRepository.findByUserId(user.getId())
+        return cartRepository.findByUserIdAndStatusNew(user.getId())
                 .orElseGet(() -> {
                     Cart newCart = new Cart();
                     newCart.setUser(user);
+                    newCart.setStatus(CartStatus.NEW);
+                    newCart.setCreatedAt(LocalDateTime.now());
                     return cartRepository.save(newCart);
                 });
     }
 
     // Sólo busca un cart en DB, segun el user autenticado
     private Cart getCartByUser(User user) {
-        return cartRepository.findByUserId(user.getId())
+        return cartRepository.findByUserIdAndStatusNew(user.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("Cart not found. User id: " + user.getId()));
     }
 }
